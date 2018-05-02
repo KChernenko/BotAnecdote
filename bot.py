f4f384d637c1226c1f872ea6505666bd03c96a0a
Constantine Chernenko <constantine.chernenko@gmail.com>
2018-05-02 14:03:44 +0300

    gTTS added, joke processing

--- bot.py
+++ bot.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
+import os
+import random
+
 import telebot
+from gtts import gTTS
 
 import config
+import persist
 
 bot = telebot.TeleBot(config.token)
 
 
-@bot.message_handler(func=lambda message: True, content_types=["text"])
+@bot.message_handler(commands=["joke"])
 def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+    text = random.choice(persist.jokes)
+    filename = "synthesized" + str(random.randint(1, 1000)) + ".mp3"
+    tts = gTTS(text=text, lang='ru')
+    tts.save(filename)
+    bot.send_audio(message.chat.id, open(filename, 'rb'))
+    os.remove(filename)
 
 
 if __name__ == '__main__':
